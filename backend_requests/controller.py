7d430bcbc213a03583c0dc69511be6bd9186218d
emil <emil.jeleskovic@tum.de>
2024-11-23 08:23:39 +0100

    shits about to hit the fan.

--- backend_requests/controller.py
+++ backend_requests/controller.py
@@ -2,10 +2,27 @@ from model.backend.ScenarioDTO import ScenarioDTO
 from model.scenario_runner.UpdateScenario import UpdateScenario
 from model.scenario_runner.VehicleUpdate import VehicleUpdate
 from scenario_runner_api import update_scenario
+import math
 
 
 def assign(scenario: ScenarioDTO) -> list[VehicleUpdate]:
-    return []
+    ret = []
+    shortest_paths = dict()
+    for v in scenario.vehicles:
+        for c in scenario.customers:
+            distance = v.remainingTravelTime * v.vehicleSpeed + math.dist((v.coordX, v.coordY), (c.coordX, c.coordY))
+            best, d = shortest_paths.get(v)
+            if not d or distance < best:
+                shortest_paths[v] = (c, distance)
+
+    for v in shortest_paths:
+        c, d = shortest_paths[v]
+        if v.isAvailable and c.awaitingService:
+            ret.append(VehicleUpdate(id=v.id, customerId=c.id))
+            c.awaitingService = False
+        else:
+            pass
+    return ret
 
 
 def step(scenario: ScenarioDTO):
