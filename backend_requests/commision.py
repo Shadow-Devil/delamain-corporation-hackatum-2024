2218cf70a10858b2847596882ef9c4daac33cf04
Xq <thomexbfsu@gmail.com>
2024-11-23 03:23:23 +0100

    tmporary push for backend

--- backend_requests/commision.py
+++ backend_requests/commision.py
@@ -2,11 +2,18 @@ from model.backend import CustomerDTO, StandardMagentaVehicleDTO
 from scipy.spatial.distance import cdist
 from scipy.optimize import linear_sum_assignment
 
-def commision(vehicals:list[StandardMagentaVehicleDTO.StandardMagentaVehicleDTO],customers:list[CustomerDTO.CustomerDTO]):
-    vehicals_coordinate = extract_coordinate(vehicals)
-    customer_coordinate = extract_coordinate(customers)
+def commission(vehicles: list[StandardMagentaVehicleDTO.StandardMagentaVehicleDTO],
+               customers: list[CustomerDTO.CustomerDTO],
+               is_inital_commision: bool):
+    # vehicle_coordinates = [(vehicle.coordX, vehicle.coordY) for vehicle in vehicles]
+    # customer_coordinates = [(customer.coordX, customer.coordY) for customer in customers]
+    # if len(vehicle_coordinates) == len(customer_coordinates):
+    #     distance_matrix = cdist(customer_coordinates, vehicle_coordinates, metric='euclidean')
+    #     customer_index,vehical_index = linear_sum_assignment(distance_matrix)
+    #     assignment = zip(vehical_index,customer_index)
+    #     for tupel in assignment:
+    #         vehicles[tupel[0]].
 
-    distance_matrix = cdist(customer_coordinate, vehicals_coordinate, metric='euclidean')
 
 
 def extract_coordinate(data): # hier können wir eine "Locatable" Interface erstellen
