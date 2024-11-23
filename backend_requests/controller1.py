471d1f4db6d5015072340a7583018021d5af74a4
Xq <thomexbfsu@gmail.com>
2024-11-23 17:25:08 +0100

    logical error by assignment in controller1 fixed

--- backend_requests/controller1.py
+++ backend_requests/controller1.py
@@ -28,65 +28,62 @@ def assign(scenario: ScenarioDTO,ratio:float) -> list[StandardMagentaVehicleDTO]
             print(index_comb)
             vehicles[index_comb[0]].customerId = customers[index_comb[1]].id
     else: # else, this algorithm will always run this part to assign taxi to one customer
-        if filter(lambda v: v.isAvailable,vehicles) == []: # the piority will increase by ratio when customers are waiting
+        if filter(lambda v: v.customerId == None,vehicles) == []: # the piority will increase by ratio when customers are waiting
             for i,c in enumerate(customers):
                 if c.awaitingService and c not in waiting_customer and c not in assigned_customer:
                     global piority_customer
                     piority_customer[i] += ratio
-        else: # if there is a taxi free now, we evaluate the distance based on the waiting list for a taxi
-            free_vehicles = filter(lambda v: v.isAvailable,vehicles)
+        else: # if there is at least one taxi free now, we evaluate the distance based on the waiting list for a taxi
+            free_vehicles = filter(lambda v: v.customerId == None,vehicles)
             for v in free_vehicles:
                 if v.id in vehicle_queue:
                     index = vehicle_queue.index(v.id)# if a vehicle is free, we check if there is already a customer waiting for this taxi
                     v.customerId = waiting_customer[index].id
                     vehicle_queue.remove(v.id)
                     waiting_customer.pop(index)
-            if free_vehicles == []:
-                return vehicles
-            else: # we assume there is now only one free vehicle falsh!
+            awaiting_customers = list()
+            awaiting_customers_index = list()
+            awaiting_customers_position = list()
+            for i,c in enumerate(customers): # recording all the waiting customer
+                if c.awaitingService and c not in waiting_customer and c not in assigned_customer:
+                    awaiting_customers.append(c)
+                    awaiting_customers_index.append(i)
+                    awaiting_customers_position.append([c.coordX,c.coordY])
+            if awaiting_customers: # if there are waiting customers
+                #print(awaiting_customers_position)
+                available_vehicles = list()
+                available_vehicles_index = list()
+                available_vehicles_position = list()
+                for i,v in enumerate(vehicles): # we take all the vehicles as long as it is working and not assigned to a waiting customer
+                    if v.id not in vehicle_queue:
+                        available_vehicles.append(v)
+                        available_vehicles_index.append(i)
+                        available_vehicles_position.append([v.coordX,v.coordY])
                 free_v_index = list()
-                for i,v in enumerate(vehicles):
-                    if v.isAvailable:
+                for i, v in enumerate(available_vehicles):  # recording all the index of free vehicles in the available vehicles
+                    if v.customerId == None:
                         free_v_index.append(i)
-                awaiting_customers = list()
-                awaiting_customers_index = list()
-                awaiting_customers_position = list()
-                for i,c in enumerate(customers):
-                    if c.awaitingService and c not in waiting_customer and c not in assigned_customer:
-                        awaiting_customers.append(c)
-                        awaiting_customers_index.append(i)
-                        awaiting_customers_position.append([c.coordX,c.coordY])
-                if awaiting_customers:
-                    #print(awaiting_customers_position)
-                    available_vehicles = list()
-                    available_vehicles_index = list()
-                    available_vehicles_position = list()
-                    for i,v in enumerate(vehicles):
-                        if v.id not in vehicle_queue:
-                            available_vehicles.append(v)
-                            available_vehicles_index.append(i)
-                            available_vehicles_position.append([v.coordX,v.coordY])
-                    #calculate weighted distance based on piority
-                    end_positions,distance_to_go = base_distance_calculator(available_vehicles,customers)
-                    distance_total = cdist(awaiting_customers_position,end_positions, metric='euclidean') + distance_to_go
-                    #print(piority_customer, awaiting_customers_index)
-                    distance_total[:,free_v_index]/=np.array(piority_customer)[awaiting_customers_index].reshape(-1,1)
-                    if len(available_vehicles)>= len(awaiting_customers):
-                        c_indices,v_indices = linear_sum_assignment(distance_total)
+                #calculate weighted distance based on piority
+                end_positions,distance_to_go = base_distance_calculator(available_vehicles,customers)
+                distance_total = cdist(awaiting_customers_position,end_positions, metric='euclidean') + distance_to_go
+                #print(piority_customer, awaiting_customers_index)
+                distance_total[:,free_v_index]/=np.array(piority_customer)[awaiting_customers_index].reshape(-1,1)
+                if len(available_vehicles)>= len(awaiting_customers):
+                    c_indices,v_indices = linear_sum_assignment(distance_total)
+                else:
+                    v_indices,c_indices = linear_sum_assignment(distance_total.T)
+                #After calculated the weighted distance, assign taxis to the best customers using the waiting list of each taxi
+                for i,v_index in enumerate(v_indices):
+                    if vehicles[available_vehicles_index[v_index]].customerId == None: # if the chosen vehicle is really free
+                        vehicles[available_vehicles_index[v_index]].customerId = customers[awaiting_customers_index[c_indices[i]]].id # assign a taxi to a customer
+                        assigned_customer.append(customers[awaiting_customers_index[c_indices[i]]])
                     else:
-                        v_indices,c_indices = linear_sum_assignment(distance_total.T)
-                    #After calculated the weighted distance, assign taxis to the best customers using the waiting list of each taxi
-                    for i,v_index in enumerate(v_indices):
-                        if vehicles[available_vehicles_index[v_index]].isAvailable:
-                            vehicles[available_vehicles_index[v_index]].customerId = customers[awaiting_customers_index[c_indices[i]]].id # assign a taxi to a customer
-                            assigned_customer.append(customers[awaiting_customers_index[c_indices[i]]])
-                        else:
-                            if vehicles[available_vehicles_index[v_index]].id in vehicle_queue:
-                                ix = vehicle_queue.index(vehicles[available_vehicles_index[v_index]].id)
-                                waiting_customer[ix].append(customers[awaiting_customers_index[c_indices[i]]])
-                            else:
-                                vehicle_queue.append(vehicles[available_vehicles_index[v_index]])
-                                waiting_customer.append([customers[awaiting_customers_index[c_indices[i]]]])
+                        #if vehicles[available_vehicles_index[v_index]].id in vehicle_queue:
+                        #    ix = vehicle_queue.index(vehicles[available_vehicles_index[v_index]].id)
+                        #    waiting_customer[ix].append(customers[awaiting_customers_index[c_indices[i]]])
+                        #else:
+                        vehicle_queue.append(vehicles[available_vehicles_index[v_index]])
+                        waiting_customer.append([customers[awaiting_customers_index[c_indices[i]]]])
     return vehicles
 
 
