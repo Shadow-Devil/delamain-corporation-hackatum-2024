91a92edcca2bd9923b9ae1b9175818d29f5a3e48
Xq <thomexbfsu@gmail.com>
2024-11-23 02:25:06 +0100

    kernal logic signature updated

--- backend_requests/commision.py
+++ backend_requests/commision.py
@@ -1,3 +1,16 @@
-from model.backend import CustomerDTO, VehicleDataDto
+from model.backend import CustomerDTO, StandardMagentaVehicleDTO
+from scipy.spatial.distance import cdist
+from scipy.optimize import linear_sum_assignment
 
-def commision(vehicals:list[VehicleDataDto.VehicleDataDto],customers:list[CustomerDTO.CustomerDTO]):
+def commision(vehicals:list[StandardMagentaVehicleDTO.StandardMagentaVehicleDTO],customers:list[CustomerDTO.CustomerDTO]):
+    vehicals_coordinate = extract_coordinate(vehicals)
+    customer_coordinate = extract_coordinate(customers)
+
+    distance_matrix = cdist(customer_coordinate, vehicals_coordinate, metric='euclidean')
+
+
+def extract_coordinate(data): # hier können wir eine "Locatable" Interface erstellen
+    coordinates = list()
+    for item in data:
+        coordinates.append([item.coordX,item.coordY])
+    return coordinates
