2dc89c1edc4a5c03c621890377db2eaadd7221b7
Felix Schnabel <f.schnabel@tum.de>
2024-11-23 06:41:53 +0100

    Fix merge conflicts

--- app.py
+++ app.py
@@ -35,8 +35,9 @@ def delete_scenario(id):
 
 @app.route("/api/scenario/", methods=["POST"])
 def create_scenario():
-    vehicles = request.args.get('vehicles', 5, int)
-    customers = request.args.get('customers', 10, int)
+    vehicles = request.form.get('vehicles', 5, int)
+    customers = request.form.get('customers', 10, int)
+    app.logger.info("vehicles %d customers %d", vehicles, customers)
     return render_template("fragment/single_scenario_in_list.html", scenario=backend.create_scenario(vehicles, customers).model_dump())
 
 @app.route("/api/scenario/<id>/launch", methods=["POST"])
