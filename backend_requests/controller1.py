73c797b1b7836371d9224012577d2c7a6cf9733c
Xq <thomexbfsu@gmail.com>
2024-11-24 03:53:53 +0100

    new calculation based on waiting time

--- backend_requests/controller1.py
+++ backend_requests/controller1.py
@@ -16,7 +16,7 @@ vehicle_queue:list[str] = []
 waiting_customer:list[CustomerDTO] = [] # assigned but waiting
 assigned_customer:list[CustomerDTO] = []
 
-def assign(scenario: ScenarioDTO,ratio:float) -> list[StandardMagentaVehicleDTO]:
+def assign(scenario: ScenarioDTO,priorityRatio:float,energyPenalty:float) -> list[StandardMagentaVehicleDTO]:
     customers = scenario.customers
     print(len(customers))
     vehicles = scenario.vehicles
@@ -28,23 +28,26 @@ def assign(scenario: ScenarioDTO,ratio:float) -> list[StandardMagentaVehicleDTO]
         for index_comb in zip(v_indices,c_indices):
             print(index_comb)
             vehicles[index_comb[0]].customerId = customers[index_comb[1]].id
-    else: # else, this algorithm will always run this part to assign taxi to one customer
-        if list(filter(lambda v: v.customerId is None,vehicles)) == []: # the piority will increase by ratio when customers are waiting
-            for i,c in enumerate(customers):
-                if c.awaitingService and c not in waiting_customer and c not in assigned_customer:
-                    global piority_customer
-                    piority_customer[i] += ratio
-        else: # if there is at least one taxi free now, we evaluate the distance based on the waiting list for a taxi
-            free_vehicles: list[StandardMagentaVehicleDTO] = list(filter(lambda v: v.customerId is None,vehicles))
-            for v in free_vehicles:
-                if v.id in vehicle_queue:
-                    index = vehicle_queue.index(v.id)# if a vehicle is free, we check if there is already a customer waiting for this taxi
-                    v.customerId = waiting_customer[index].id
-                    vehicle_queue.pop(index)
-                    waiting_customer.pop(index)
-                    print("removed from waiting but assigned list:", len(waiting_customer))
-            print("current vehicle queue:", len(vehicle_queue))
-            print("waiting but assigned: ", len(waiting_customer))
+    else: # else, the algorithm will always run this part to assign taxi
+        #if list(filter(lambda v: v.customerId is None,vehicles)) == []: # the piority will increase by ratio when customers are waiting
+        for i,c in enumerate(customers):
+            if c.awaitingService and c not in waiting_customer and c not in assigned_customer:
+                global piority_customer
+                piority_customer[i] += priorityRatio
+        #else: # if there is at least one taxi free now, we evaluate the distance based on the waiting list for a taxi
+        free_vehicles: list[StandardMagentaVehicleDTO] = list(filter(lambda v: v.customerId is None,vehicles))
+        for v in free_vehicles:
+            if v.id in vehicle_queue:
+                index = vehicle_queue.index(v.id)# if a vehicle is free, we check if there is already a customer waiting for this taxi
+                v.customerId = waiting_customer[index].id
+                assigned_customer.append(waiting_customer[index])
+                vehicle_queue.pop(index)
+                waiting_customer.pop(index)
+                print("current length of waiting but assigned list:", len(waiting_customer))
+        print("current vehicle queue:", len(vehicle_queue))
+        print("waiting but assigned: ", len(waiting_customer))
+        free_vehicles = list(filter(lambda v: v.customerId is None,vehicles))
+        if len(free_vehicles)>0:
             awaiting_customers = list()
             awaiting_customers_index = list()
             awaiting_customers_position = list()
@@ -68,10 +71,12 @@ def assign(scenario: ScenarioDTO,ratio:float) -> list[StandardMagentaVehicleDTO]
                     if v.customerId is None:
                         free_v_index.append(i)
                 #calculate weighted distance based on piority
-                end_positions,distance_to_go = base_distance_calculator(available_vehicles,customers)
-                distance_total = cdist(awaiting_customers_position,end_positions, metric='euclidean') + distance_to_go
-                #print(piority_customer, awaiting_customers_index)
+                end_positions,time_to_go = base_distance_calculator(available_vehicles,customers)
+                print(time_to_go)
+                distance_total = cdist(awaiting_customers_position,end_positions, metric='euclidean')*185.2216 + time_to_go #10 is estimated average speed
                 distance_total[:,free_v_index]/=np.array(piority_customer)[awaiting_customers_index].reshape(-1,1)
+                distanceTravelledTotal = np.array(list(map(lambda v:v.distanceTravelled,available_vehicles)))/energyPenalty
+                distance_total += distanceTravelledTotal
                 if len(available_vehicles)>= len(awaiting_customers):
                     c_indices,v_indices = linear_sum_assignment(distance_total)
                 else:
@@ -82,10 +87,6 @@ def assign(scenario: ScenarioDTO,ratio:float) -> list[StandardMagentaVehicleDTO]
                         vehicles[available_vehicles_index[v_index]].customerId = customers[awaiting_customers_index[c_indices[i]]].id # assign a taxi to a customer
                         assigned_customer.append(customers[awaiting_customers_index[c_indices[i]]])
                     else:
-                        #if vehicles[available_vehicles_index[v_index]].id in vehicle_queue:
-                        #    ix = vehicle_queue.index(vehicles[available_vehicles_index[v_index]].id)
-                        #    waiting_customer[ix].append(customers[awaiting_customers_index[c_indices[i]]])
-                        #else:
                         vehicle_queue.append(vehicles[available_vehicles_index[v_index]].id)
                         waiting_customer.append(customers[awaiting_customers_index[c_indices[i]]])
     return vehicles
@@ -95,20 +96,31 @@ def base_distance_calculator(vehicles,customers):
     end_positions = list()
     distance_to_go = list()
     for v in vehicles:
-        if v.customerId == None:
+        if v.customerId is None:
             end_positions.append([v.coordX,v.coordY])
             distance_to_go.append(0)
         else:
             customer_assigned = list(filter(lambda c: c.id == v.customerId,customers))
             end_positions.append([customer_assigned[0].destinationX,customer_assigned[0].destinationX])
-            distance_to_go.append(np.linalg.norm(np.array([v.coordX,v.coordY]-np.array([customer_assigned[0].destinationX,customer_assigned[0].destinationX]))))
+            if v.vehicleSpeed == 0:
+                distance_to_go.append(0)
+            else:
+                print("first distance: ", np.linalg.norm(np.array([v.coordX, v.coordY] - np.array(
+                        [customer_assigned[0].coordX, customer_assigned[0].coordY]))))
+                print("seconde distance: ",np.linalg.norm(np.array([v.coordX,v.coordY]-np.array([customer_assigned[0].destinationX,customer_assigned[0].destinationY]))))
+                distance_to_go.append(
+                    (np.linalg.norm(np.array([v.coordX,v.coordY]-np.array([customer_assigned[0].destinationX,customer_assigned[0].destinationY])))+
+                    np.linalg.norm(np.array([v.coordX, v.coordY] - np.array(
+                        [customer_assigned[0].coordX, customer_assigned[0].coordY]))))* 1852.216/v.vehicleSpeed
+                )
+
     return end_positions, distance_to_go
 
 def step(scenario: ScenarioDTO):
     customers = list(filter(lambda c: c.awaitingService, scenario.customers))
     updates: List[StandardMagentaVehicleDTO] = []
     if customers and any([v.isAvailable for v in scenario.vehicles]):
-        updates = assign(scenario,0.5)
+        updates = assign(scenario,0.1,10000)
     updates_ = list(filter(lambda x: x.customerId is not None, map(lambda x: VehicleUpdate.model_validate(x.model_dump()), updates)))
     #print(str(updates_))
     update_scenario(scenario.id, UpdateScenario(vehicles=updates_))
